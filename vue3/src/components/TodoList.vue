db82f8577720bc57cd64216dd1d29c1e2479cb39
DingX2 <pourding@naver.com>
2024-07-03 00:46:14 +0900

    feat(todolist): todolist 컴포넌트 구현

---
+++ vue3/src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+// Composition API 사용
+export default defineComponent({
+  name: 'TodoList',
+  setup() {
+    const todos = ref([
+      { id: 1, text: '더미데이터' },
+      { id: 2, text: '더미데이터2' }
+    ]);
+
+    return {
+      todos
+    };
+  }
+});
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">{{ todo.text }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+</style>
