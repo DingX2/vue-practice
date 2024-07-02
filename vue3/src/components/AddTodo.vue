677fbc92a7723198db707dbc015c5f7084076b81
DingX2 <pourding@naver.com>
2024-07-03 01:09:37 +0900

    feat(addtodo): addtodo 구현

---
+++ vue3/src/components/AddTodo.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { defineComponent, ref } from 'vue';
+
+  export default defineComponent({
+    name: 'AddTodo',
+    emits: ['add-todo'],
+    setup(_, { emit }) {
+    const newTodo = ref('');
+
+    const emitAddTodo = () => {
+      if (newTodo.value.trim() !== '') {
+        emit('add-todo', newTodo.value);  // 부모 컴포넌트로 'add-todo' 이벤트 발송
+        newTodo.value = '';  // 입력 필드 초기화
+      }
+    };
+
+    return {
+      newTodo,
+      emitAddTodo
+    };
+  }
+})
+</script>
+
+<template>
+  <div>
+    <!-- v-model 양방향 바인딩 구현 -->
+    <input type="text" v-model="newTodo" placeholder="add Todo"/>
+    <button @click="emitAddTodo">Add Todo</button>
+  </div>
+</template>
+
+<style>
+</style>
